8d91cee0e257a0667099317d998e95b4073e1105
Lorenzo Corbella <lcorbella@expert.ai>
2020-11-09 14:08:59 +0100

    new page

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const CANVAS_DIMENSIONS = {
+    WIDTH: 4000,
+    HEIGHT: 3000
+}
 
 let windowWidth = 0;
 let windowHeight = 0;
-
-const footer = document.getElementById('footer');
 
 const pointerStatus = {
     PAN: "PAN",
@@ -14,27 +14,16 @@ const pointerStatus = {
 
 let SNAP_TO_GRID = true;
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
 const captureMouse = function (element) {
-    var mouse = { x: 0, y: 0, event: null },
-        body_scrollLeft = document.body.scrollLeft,
-        element_scrollLeft = document.documentElement.scrollLeft,
-        body_scrollTop = document.body.scrollTop,
-        element_scrollTop = document.documentElement.scrollTop,
-        offsetLeft = element.offsetLeft,
-        offsetTop = element.offsetTop;
+    var mouse = { x: 0, y: 0, event: null };
 
     element.addEventListener('mousemove', function (event) {
-        var x, y;
 
-        if (event.pageX || event.pageY) {
-            x = event.pageX;
-            y = event.pageY;
-        } else {
-            x = event.clientX + body_scrollLeft + element_scrollLeft;
-            y = event.clientY + body_scrollTop + element_scrollTop;
-        }
-        x -= offsetLeft;
-        y -= offsetTop;
+        let x = parseInt(event.clientX);
+        let y = parseInt(event.clientY);
 
         if (SNAP_TO_GRID) {
             let restoH = x % 20;
@@ -62,14 +51,14 @@ const captureMouse = function (element) {
 let mouse = captureMouse(canvas);
 
 function resizeCanvas () {
-    canvas.width = window.innerWidth * 0.8;     // 20% is the sidebar
-    canvas.height = window.innerHeight - 34;    // footer height
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     windowWidth = canvas.width;
     windowHeight = canvas.height;
-    draw();
+    drawAll();
 }
 
-function draw () {
+function drawAll () {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     drawCanvas();
@@ -83,21 +72,23 @@ function drawPointer () {
     ctx.lineWidth = 0.5;
     ctx.beginPath();
     ctx.moveTo(mouse.x, 0);
-    ctx.lineTo(mouse.x, windowHeight);
+    ctx.lineTo(mouse.x, CANVAS_DIMENSIONS.HEIGHT);
     ctx.moveTo(0, mouse.y);
-    ctx.lineTo(windowWidth, mouse.y);
+    ctx.lineTo(CANVAS_DIMENSIONS.WIDTH, mouse.y);
     ctx.closePath();
     ctx.stroke();
+    ctx.fillStyle = "grey";
+    ctx.fillText(`x: ${mouse.x} - y: ${mouse.y}`, mouse.x + 10, mouse.y + 10)
 }
 
 function drawCanvas () {
     ctx.fillStyle = "rgb(31,40,49)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     // colonne
-    for (i = 0; i < windowWidth; i += 20) {
+    for (let i = 0; i < CANVAS_DIMENSIONS.WIDTH; i += 20) {
         ctx.beginPath();
         ctx.moveTo(i + 0.5, 0);
-        ctx.lineTo(i + 0.5, windowHeight);
+        ctx.lineTo(i + 0.5, CANVAS_DIMENSIONS.HEIGHT);
         if (i % 100 === 0) {
             ctx.strokeStyle = "rgb(48,55,71)";
         } else {
@@ -106,13 +97,19 @@ function drawCanvas () {
         ctx.lineWidth = 0.5;
         ctx.closePath()
         ctx.stroke();
+        if (i % 100 === 0) {
+            ctx.font = "10px Arial";
+            ctx.fillStyle = "grey";
+            // ctx.textAlign = "center";
+            ctx.fillText(i.toString(), i + 2.5, 10);
+        }
     }
     // righe
-    for (i = windowHeight; i > 0; i -= 20) {
+    for (let i = 0; i < CANVAS_DIMENSIONS.HEIGHT; i += 20) {
         ctx.beginPath();
         ctx.moveTo(0, i + 0.5);
         ctx.lineTo(windowWidth, i + 0.5);
-        if ((windowHeight - i) % 100 === 0) {
+        if (i % 100 === 0) {
             ctx.strokeStyle = "rgb(48,55,71)";
         } else {
             ctx.strokeStyle = "rgb(36,45,56)";
@@ -120,7 +117,39 @@ function drawCanvas () {
         ctx.lineWidth = 0.5;
         ctx.closePath()
         ctx.stroke();
+        if (i % 100 === 0) {
+            ctx.font = "10px Arial";
+            ctx.fillStyle = "grey";
+            // ctx.textAlign = "center";
+            ctx.fillText(i.toString(), 2.5, i - 2.5);
+        }
     }
+}
+
+// mouse drag related variables
+var isDown = false;
+var startX, startY;
+
+// the accumulated horizontal(X) & vertical(Y) panning the user has done in total
+var netPanningX = 0;
+var netPanningY = 0;
+
+function begin () {
+    ctx.save();
+    applyScale();
+    applyTranslation();
+}
+
+function end () {
+    ctx.restore();
+}
+
+function applyScale () {
+    ctx.scale(1, 1/* this.viewport.scale[0], this.viewport.scale[1] */);
+}
+
+function applyTranslation () {
+    ctx.translate(netPanningX, netPanningY);
 }
 
 window.onload = function () {
@@ -128,10 +157,75 @@ window.onload = function () {
     // resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
 
-    canvas.addEventListener('mousemove', function () {
-        footer.textContent = `> x: ${mouse.x} - y: ${mouse.y}`;
-        draw();
+    canvas.addEventListener('mousemove', (e) => {
+
+        // only do this code if the mouse is being dragged
+        if (isDown) {
+            // get the current mouse position
+            let mouseX = parseInt(e.clientX);
+            let mouseY = parseInt(e.clientY);
+
+            // dx & dy are the distance the mouse has moved since
+            // the last mousemove event
+            var dx = mouseX - startX;
+            var dy = mouseY - startY;
+
+            // reset the vars for next mousemove
+            startX = mouseX;
+            startY = mouseY;
+
+            // accumulate the net panning done
+            netPanningX += dx;
+            netPanningY += dy;
+            console.clear()
+            console.log(`Net change in panning: x:${netPanningX}px, y:${netPanningY}px`);
+        }
+
+        // tell the browser we're handling this event
+        e.preventDefault();
+        e.stopPropagation();
+
+        // begin()
+        drawAll();
+        // end()
     }, false);
+
+    canvas.addEventListener('mousedown', function (e) {
+        // tell the browser we're handling this event
+        e.preventDefault();
+        e.stopPropagation();
+
+        // calc the starting mouse X,Y for the drag
+        startX = parseInt(e.clientX);
+        startY = parseInt(e.clientY);
+
+        // set the isDragging flag
+        isDown = true;
+    }, false);
+
+    canvas.addEventListener('mouseup', function (e) {
+        // tell the browser we're handling this event
+        e.preventDefault();
+        e.stopPropagation();
+
+        // clear the isDragging flag
+        isDown = false;
+        netPanningX = 0;
+        netPanningY = 0;
+    }, false);
+
+
+    canvas.addEventListener('mouseout', function (e) {
+        // tell the browser we're handling this event
+        e.preventDefault();
+        e.stopPropagation();
+
+        // clear the isDragging flag
+        isDown = false;
+        netPanningX = 0;
+        netPanningY = 0;
+    }, false);
+
 
     resizeCanvas();
 };
