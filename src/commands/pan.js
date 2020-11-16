d077748206c586023125d7cce00eecaf1d6ee5dd
Lorenzo Corbella <ing.lorenzo.corbella@g,ail.com>
2020-11-17 00:10:51 +0100

    zoom command

--- src/commands/pan.js
+++ src/commands/pan.js
@@ -6,7 +6,7 @@ export default class PanCommand extends Command {
         super(state)
 
         // this.mouse drag related variables
-        this.isDown = false;
+        this.isMouseDown = false;
         this.startX, this.startY;
 
         // the accumulated horizontal(X) & vertical(Y) panning the user has done in total
@@ -15,16 +15,11 @@ export default class PanCommand extends Command {
     }
 
     mousemove(e) {
-
-        // tell the browser we're handling this event
-        e.preventDefault();
-        e.stopPropagation();
-
         let x = e._x;
         let y = e._y;
 
         // if the this.mouse is being dragged
-        if (this.isDown) {
+        if (this.isMouseDown) {
             // dx & dy are the distance the this.mouse has moved since the last this.mousemove event
             var dx = x - this.startX;
             var dy = y - this.startY;
@@ -36,40 +31,32 @@ export default class PanCommand extends Command {
             // accumulate the net panning done
             this.main.netPanningX += dx;
             this.main.netPanningY += dy;
-            console.clear()
-            console.log(`Net change in panning: x:${this.main.netPanningX}px, y:${this.main.netPanningY}px`);
+            // console.clear()
+            // console.log(`Net change in panning: x:${this.main.netPanningX}px, y:${this.main.netPanningY}px`);
         }
+
         this.main.mouse.x = x;
         this.main.mouse.y = y;
         this.main.mouse.event = e;
     }
 
     mousedown(e) {
-        e.preventDefault();
-        e.stopPropagation();
-
         // calc the starting this.mouse X,Y for the drag
         this.startX = e._x;
         this.startY = e._y;
 
         // set the isDragging flag
-        this.isDown = true;
+        this.isMouseDown = true;
     }
 
     mouseup(e) {
-        console.log('PanCommand: mouseup', e, this)
-        e.preventDefault();
-        e.stopPropagation();
         // clear the isDragging flag
-        this.isDown = false;
+        this.isMouseDown = false;
     }
 
     mouseout(e) {
-        console.log('PanCommand: mouseout', e, this)
-        e.preventDefault();
-        e.stopPropagation();
         // clear the isDragging flag
-        this.isDown = false;
+        this.isMouseDown = false;
     }
 
 }
